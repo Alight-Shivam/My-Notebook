40125a85b03d09a8b4c348d9d61239d842878d1e
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-15 16:32:07 +0530

    Project in progress

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/?directConnection=true"
+const mongoURI = "mongodb://127.0.0.1:27017/?directConnection=true"
 
 const connectToMongo = async ()=>{
     mongoose.connect(mongoURI, await console.log("Connected To Mongo Successfully")
