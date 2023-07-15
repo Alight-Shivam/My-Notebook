8b902077fc940f6e9763bfb5d50cc3869f4e9da8
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-15 18:00:47 +0530

    Project in progress

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 
 //Create a User using: POST "/api/auth/". Doesn't require Auth
-router.get('/', (req,res)=>{
-   console.log(req.body);
+router.post('/',[
+    body('password').isLength({ min: 5 }),
+    body('email').isEmail(),
+    body('name').isLength({min: 3 })
+], (req,res)=>{
+   const errors = validationResult(req);
+   if(!errors.isEmpty()){
+    return res.status(400).json({errors: errors.array() });
+   }
+   res.send(req.body);
+    
 })
 
 module.exports = router
