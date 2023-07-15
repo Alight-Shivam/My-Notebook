fbe28c97200cf393e85e003f868974c0eb8115c5
Alight-Shivam <97473543+Alight-Shivam@users.noreply.github.com>
2023-07-15 14:34:39 +0530

    project in progress

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const mongoURI = "mongodb://localhost:27017/?directConnection=true"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("Connected To Mongo Successfully");
-    })
+const connectToMongo = async ()=>{
+    mongoose.connect(mongoURI, await console.log("Connected To Mongo Successfully")
+    );
+    
 }
 
 module.exports = connectToMongo;
